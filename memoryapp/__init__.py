17168df8fbf61f6b2f6bab78b5711bf70fc1e8c7
Arek <a.stoszek@!gmail.com>
2025-05-24 12:00:08 +0200

    Przygotowanie bazy danych

--- memoryapp/__init__.py
+++ memoryapp/__init__.py
@@ -1,6 +1,31 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event, Engine
+from sqlalchemy.orm import DeclarativeBase
 
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.debug = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///memory.db'
+app.config['SQLALCHEMY_ECHO'] = True
+
+
+db.init_app(app)
+
+@event.listens_for(Engine, 'connect')
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+with app.app_context():
+    db.create_all()
+
