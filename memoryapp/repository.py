9cb2f0a3f46b3fb6b98c395bfb24f66d03dbec4b
Arek <a.stoszek@!gmail.com>
2025-05-24 14:42:25 +0200

    Tworzenie kategorii

--- memoryapp/repository.py
+++ memoryapp/repository.py
@@ -1,7 +1,14 @@
+from memoryapp import db
 from memoryapp.entities import Category
 
 
 def get_categories() -> list[Category]:
     return Category.query.all()
 
+def create_category(category_name: str) -> Category:
+    category = Category(category_name=category_name)
 
+    db.session.add(category)
+    db.session.commit()
+
+    return category
--- memoryapp/routes/categories.py
+++ memoryapp/routes/categories.py
@@ -1,10 +1,18 @@
-from flask import jsonify
+from flask import jsonify, request
+
 
 from memoryapp import app
-from memoryapp.repository import get_categories
+from memoryapp.repository import get_categories, create_category
 
 
 @app.route('/categories', methods=['GET'])
 def categories():
     return jsonify(get_categories())
 
+
+@app.route('/categories', methods=['POST'])
+def add_category():
+    r = request.json
+    category_name = r['category_name']
+
+    return jsonify(create_category(category_name)), 201
